7f1ede5c98cf8505563ff5b96bb937da36103208
aaronlacey <aaron.lacey@codeimmersives.com>
2022-10-25 10:28:27 -0700

    update

---
+++ main.js
@@ -0,0 +1,36 @@
+// change the color of the paragraph text to light blue
+
+let p = document.querySelector('p');
+p.style.color = 'lightblue';
+
+//// change the paragraph text to be hipster ipsum
+
+p.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.';
+
+
+let item13 = document.querySelector("#item-13");
+item13.style.opacity = 0.5;
+
+let item3 = document.querySelector("#item-3");
+item3.innerText = "I SAY, Hi!";
+
+
+let h1 = document.querySelector('h1');
+h1.style.fontSize = '48px';
+
+
+let img = document.querySelector('img');
+img.src = "https://res.cloudinary.com/dtpgi0zck/image/upload/s--SsFGdDoP--/c_fill,h_580,w_860/v1/EducationHub/photos/ocean-waves.jpg";
+
+img.style.height = '300px';
+
+// give image2 a query selcetor
+
+let img2 = document.querySelector('#img2');
+img2.style.height = '300px';
+
+
+let item16 = document.querySelector('#item-16');
+item16.className = "item";
+
+item16.innerText = "Won't get fooled again";
